fafd727e7ad78a23eba4ed4083966e0ab7999d54
leo <mutongbaobao@gmail.com>
2026-09-15 16:44:54 +0800

    fixup! component-overrides: Add google components (Google Quick Search Box)

--- config/common.mk
+++ config/common.mk
@@ -54,10 +54,13 @@ endif
 PRODUCT_COPY_FILES += \
     vendor/custom/config/permissions/privapp-permissions-lineagehw.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-lineagehw.xml
 
-# Google Photos Pixel Exclusive XML & Google Quick Search Box Overrides
+# Google Photos Pixel Exclusive XML
 PRODUCT_COPY_FILES += \
-    vendor/custom/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml \
-    vendor/custom/prebuilt/common/etc/sysconfig/pixel-component-overrides.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel-component-overrides.xml
+    vendor/custom/prebuilt/common/etc/sysconfig/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
+
+# Component overrides (Google Quick Search Box)
+PRODUCT_PACKAGES += \
+    pixel-component-overrides.xml
 
 # Overlay
 PRODUCT_PACKAGE_OVERLAYS += \
